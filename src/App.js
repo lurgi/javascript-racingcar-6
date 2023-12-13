4c9313110aed524fb33496fb218a80605deb7725
lurgi <wahoo9040@naver.com>
2023-12-13 11:02:30 +0900

    docs(README): 기능 구현 목록

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 class App {
-  async play() {}
+  async play() {
+    //TODO 자동차 이름 입력받기
+    /**
+     * 1. 자동차 경주 클래스 만들기
+     * 2. 자동차 이름 입력받기.
+     * 3. 자동차 이름으로 자동차 클래스 만들고 경주 객체에 담기
+     */
+
+    //TODO 횟수 입력받기
+    //TODO 실행 결과 출력하기
+    //TODO 우승자 출력하기
+  }
 }
 
 export default App;
