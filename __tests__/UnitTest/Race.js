e7a970a423b6fb154d72c681209578d2d5ecfaf5
lurgi <wahoo9040@naver.com>
2023-12-13 11:55:24 +0900

    test(Race): 테스트 추가

--- __tests__/UnitTest/Race.js
+++ __tests__/UnitTest/Race.js
@@ -2,8 +2,13 @@ import Race from '../../src/model/Race';
 
 /* eslint-disable */
 describe('Race 클래스', () => {
-  test.each(['eeeeee,rq', '1', ',,'])('setCars 에러', (input) => {
+  test.each(['eeeeee,rq', '1', ',,', '123,123'])('setCars 에러', (input) => {
     const RACE = new Race();
     expect(() => RACE.setCars(input)).toThrow('[ERROR]');
+  });
+
+  test.each([NaN, -5, 51])('setTry 에러', (input) => {
+    const RACE = new Race();
+    expect(() => RACE.setTry(input)).toThrow('[ERROR]');
   });
 });
